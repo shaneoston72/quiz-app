8e158338046af9a890f02a270321191099bd3c5f
Shane Oston Stowe <shane@shaneoston.uk>
2017-05-09 14:27:27 +0100

    refactor questions object
    refactor to an object of objects

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var state = {
 
 };
 
-var questions = [
-    {
+var questions = {
+    1: {
         question: 'Question 1',
         answers: {
             a: 'answer 1',
@@ -17,7 +17,7 @@ var questions = [
         },
         answer: 'a'
     },
-    {
+    2: {
         question: 'Question 2',
         answers: {
             a: 'answer 1',
@@ -27,7 +27,7 @@ var questions = [
         },
         answer: 'b'
     },
-    {
+    3: {
         question: 'Question 3',
         answers: {
             a: 'answer 1',
@@ -37,7 +37,7 @@ var questions = [
         },
         answer: 'c'
     },
-    {
+    4: {
         question: 'Question 4',
         answers: {
             a: 'answer 1',
@@ -47,45 +47,45 @@ var questions = [
         },
         answer: 'd'
     }
-];
+};
 
-var gameTemplate = [
-    '<div class="game-progress">' +
-        '<p class="game-progress">Question 7 out of 10</p>' +
-    '</div>' +
-    '<div class="game-question">' +
-        '<p class="question">This is a question.</p>' +
-    '</div>' +
-    '<div>' +
-        '<div class="game-answer">' +
-            '<label for="answer">Select your answer</label>' +
-            '<select name="answer">' +
-                '<option value="a">A</option>' +
-                '<option value="b">B</option>' +
-                '<option value="c">C</option>' +
-                '<option value="d">D</option>' +
-            '</select>' +
-        '</div>' +
-        '<div class="game-answer-response">' +
-            '<div class="incorrect-answer">' +
-                '<p>Your answer is incorrect.</p>' +
-                '<p>The correct answer is A.</p>' +
-            '</div>' +
-            '<div class="correct-answer">' +
-                '<p class="correct-answer">Your answer is correct.</p>' +
-            '</div>' +
-        '</div>' +
-    '</div>' +
-    '<div class="game-score">' +
-        '<p class="game-score">2 correct, 5 incorrect</p>' +
-    '</div>' +
-    '<div class="game-result">' +
-        '<p>Game over. Your score is 5 out of X</p>' +
-    '</div>' +
-    '<section>' +
-        '<button type="button">Start a new game</button>' +
-    '</section>'
-];
+// var gameTemplate = [
+//     '<div class="game-progress">' +
+//         '<p class="game-progress">Question 7 out of 10</p>' +
+//     '</div>' +
+//     '<div class="game-question">' +
+//         '<p class="question">This is a question.</p>' +
+//     '</div>' +
+//     '<div>' +
+//         '<div class="game-answer">' +
+//             '<label for="answer">Select your answer</label>' +
+//             '<select name="answer">' +
+//                 '<option value="a">A</option>' +
+//                 '<option value="b">B</option>' +
+//                 '<option value="c">C</option>' +
+//                 '<option value="d">D</option>' +
+//             '</select>' +
+//         '</div>' +
+//         '<div class="game-answer-response">' +
+//             '<div class="incorrect-answer">' +
+//                 '<p>Your answer is incorrect.</p>' +
+//                 '<p>The correct answer is A.</p>' +
+//             '</div>' +
+//             '<div class="correct-answer">' +
+//                 '<p class="correct-answer">Your answer is correct.</p>' +
+//             '</div>' +
+//         '</div>' +
+//     '</div>' +
+//     '<div class="game-score">' +
+//         '<p class="game-score">2 correct, 5 incorrect</p>' +
+//     '</div>' +
+//     '<div class="game-result">' +
+//         '<p>Game over. Your score is 5 out of X</p>' +
+//     '</div>' +
+//     '<section>' +
+//         '<button type="button">Start a new game</button>' +
+//     '</section>'
+// ];
 
 // state modification
 
