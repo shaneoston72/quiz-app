d65148c866a0d3ed1b73d4ecc1aa3ccf3217eef9
Shane Oston Stowe <shane@shaneoston.uk>
2017-05-14 12:56:07 +0100

    * completed MVP that meets general requirements.

--- js/app.js
+++ js/app.js
@@ -9,8 +9,16 @@
 var state = {
     currentQuestion: 0,
     currentAnswer: '',
+    submittedAnswer: '',
     correctAnswers: 0,
-    incorrectAnswers: 0
+    incorrectAnswers: 0,
+    reset: function() {
+        this.currentQuestion = 0;
+        this.currentAnswer = '';
+        this.submittedAnswer = '';
+        this.correctAnswers = 0;
+        this.incorrectAnswers =  0;
+    }
 };
 
 var board = {
@@ -28,14 +36,19 @@ var board = {
     choice: function(choiceKey, choiceValue) {
         return '<li>' +
                     '<input type="radio" name="choice" value="' + choiceKey + '" required>' +
-                    '<label>' + choiceValue + '</label>' +
+                    '<label>' + choiceKey.toUpperCase() + '.  ' + choiceValue + '</label>' +
                 '</li>';
     },
     correctAnswer: function () {
         return 'That\'s right';
     },
     incorrectAnswer: function (state, questions) {
-        return 'Oops. The correct answer is '
+        console.log(state);
+        return 'Oops. The correct answer is ' + questions[state.currentQuestion - 1].answer.toUpperCase();
+    },
+    result: function(state, questionsObj) {
+        return 'Game Over. <br/> Your final score is ' + state.correctAnswers + ' correct ' + pluralise(state.score, 'answer') +
+        ' out of ' + questionsObj.length + ' questions.';
     }
 };
 /**
@@ -52,7 +65,8 @@ var questions = [
             d: 'President Trump\'s Secret Service Director'
         },
         answer: 'a'
-    },
+    }
+    ,
     {
         question: 'Question 2',
         choices: {
@@ -63,42 +77,41 @@ var questions = [
         },
         answer: 'b'
     }
-    ,
-    {
-        question: 'Question 3',
-        choices: {
-            a: 'answer 1',
-            b: 'answer 2',
-            c: 'answer 3',
-            d: 'answer 4'
-        },
-        answer: 'c'
-    },
-    {
-        question: 'Question 4',
-        choices: {
-            a: 'answer 1',
-            b: 'answer 2',
-            c: 'answer 3',
-            d: 'answer 4'
-        },
-        answer: 'd'
-    },
-    {
-        question: 'Question 5',
-        choices: {
-            a: 'answer 1',
-            b: 'answer 2',
-            c: 'answer 3',
-            d: 'answer 4'
-        },
-        answer: 'a'
-    }
+    // ,
+    // {
+    //     question: 'Question 3',
+    //     choices: {
+    //         a: 'answer 1',
+    //         b: 'answer 2',
+    //         c: 'answer 3',
+    //         d: 'answer 4'
+    //     },
+    //     answer: 'c'
+    // },
+    // {
+    //     question: 'Question 4',
+    //     choices: {
+    //         a: 'answer 1',
+    //         b: 'answer 2',
+    //         c: 'answer 3',
+    //         d: 'answer 4'
+    //     },
+    //     answer: 'd'
+    // },
+    // {
+    //     question: 'Question 5',
+    //     choices: {
+    //         a: 'answer 1',
+    //         b: 'answer 2',
+    //         c: 'answer 3',
+    //         d: 'answer 4'
+    //     },
+    //     answer: 'a'
+    // }
 ];
 
 // ---------------------------------------------------------------------------------------------------
-// State management
-
+// HELPER FUNCTIONS
 /**
  * getQuestion return a question from questions object after checking state for the current question
  * @param state
@@ -106,7 +119,7 @@ var questions = [
  * @returns {*|object}
  */
 function getQuestion(state, questionsObj) {
-    return state.currentQuestion === 0 ? questionsObj[0] : questionsObj[state.currentQuestion + 1];
+    return state.currentQuestion === 0 ? questionsObj[0] : questionsObj[state.currentQuestion];
 }
 
 function getChoicesHTML(choices) {
@@ -121,8 +134,18 @@ function getChoicesHTML(choices) {
     return choicesRadioButtons.join('');
 }
 
-function checkSubmittedResponse(state, choice, questions) {
-    if (choice === questions[state.currentQuestion].answer) {
+function pluralise(number, word) {
+    var plural = '';
+    if (number === 0 || number > 0 ) {
+        plural = word + 's';
+    }
+    return (number === 1 ? word : plural);
+}
+
+// ---------------------------------------------------------------------------------------------------
+// State management
+function checkSubmittedResponse(state, choice) {
+    if (choice === state.currentAnswer) {
         state.correctAnswers++;
         return true;
     }
@@ -130,6 +153,14 @@ function checkSubmittedResponse(state, choice, questions) {
     state.incorrectAnswers++;
 
     return false;
+}
+
+function setCurrentAnswer(question) {
+    state.currentAnswer = question.answer;
+}
+
+function incrementQuestion(state) {
+    state.currentQuestion++;
 }
 
 // ---------------------------------------------------------------------------------------------------
@@ -164,32 +195,85 @@ function startRoundOfTheGame() {
     showProgress($('#game-progress'));
     showScore($('#game-score'));
     showQuestionAndChoices($('#game-question'), $('#game-choices ul'));
+    hideElement($('#game-answer-response'));
 }
 
         function showProgress(element) {
             element.html(board.progress(state, questions));
+            showElement(element);
         }
 
         function showScore(element) {
             element.html(board.score(state));
+            showElement(element);
         }
 
         function showQuestionAndChoices(questionElement, choicesElement) {
             var question = getQuestion(state, questions),
                 choices = question.choices;
 
+            setCurrentAnswer(question);
+
             questionElement.html(board.question(question));
             choicesElement.html(getChoicesHTML(choices));
+
+            incrementQuestion(state);
         }
 
-function processChoice(choice) {
-    var result = checkSubmittedResponse(state, choice, questions);
+function processChoice(choice, submitButton) {
+    answerText(choice);
 
+    hideElement(submitButton);
 
-    console.log(state);
+    showElement($('#game-answer-response'));
+
+    showScore($('#game-score'));
+
+    setTimeout(function() {
+        nextRound(submitButton, $('#game-answer-response'));
+    }, 1500);
 }
 
+        function answerText(choice) {
+            var result = checkSubmittedResponse(state, choice);
 
+            if (result === true) {
+                $('#game-answer-response').html(board.correctAnswer());
+            } else {
+                $('#game-answer-response').html(board.incorrectAnswer(state, questions));
+            }
+        }
+
+function nextRound(buttonElement, answerResponseElement) {
+    hideElement(answerResponseElement);
+
+    if (state.currentQuestion === questions.length) {
+        toggleVisibility($('#game-box'));
+        showElement($('#game-result'));
+        $('#result').html(board.result(state, questions));
+    } else {
+        startRoundOfTheGame();
+        showElement(buttonElement);
+    }
+}
+
+function startNewGame(state, gameBox) {
+    state.reset();
+
+    hideElement($('#game-result'));
+
+    showElement($('#submit-button'));
+
+    openTheGameBox(gameBox);
+}
+
+function hideElement(element) {
+    element.hide();
+}
+
+function showElement(element) {
+    element.show();
+}
 // ---------------------------------------------------------------------------------------------------
 // EVENT LISTENERS
 
@@ -212,9 +296,13 @@ function handleChoice(submitButton) {
 
         var choice = $("input[name='choice']:checked").val();
 
-        toggleVisibility(submitButton);
+        processChoice(choice, submitButton);
+    });
+}
 
-        processChoice(choice);
+function newGame(newGameButton, gameBox) {
+    newGameButton.click(function() {
+        startNewGame(state, gameBox);
     });
 }
 
@@ -222,9 +310,11 @@ function handleChoice(submitButton) {
 $(function() {
     var gameBox = $('#game-box'),
         startButton = $('#start-button'),
-        submitButton = $('#submit-button');
+        submitButton = $('#submit-button'),
+        newGameButton = $('#new-game');
 
 
     startTheGame(startButton, gameBox);
     handleChoice(submitButton);
+    newGame(newGameButton, gameBox);
 });
